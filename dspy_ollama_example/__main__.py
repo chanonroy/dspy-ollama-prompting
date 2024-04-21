b63cf0593b51b968f3c4710a8df60abb3eea2d11
Chanon Roy <chanonroy@gmail.com>
2024-04-22 00:56:29 +0100

    add output as a comment

--- dspy_ollama_example/__main__.py
+++ dspy_ollama_example/__main__.py
@@ -7,4 +7,10 @@ dspy.settings.configure(lm=llama3)
 
 module = FindTerms()
 response = module("Their jaws and wallets will drop to the floor. Watch out for the godfather.")
-print(response)    
+
+print(response)
+
+"""
+Prompt: Identify up to 3 terms in the following question that are famous movie names. Their jaws and wallets will drop to the floor. Watch out for the godfather.
+Terms: ['Jaws', 'Godfather']
+"""
